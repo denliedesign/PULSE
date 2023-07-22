0e6c9a5baecfd2b874dc7a7b4a8ab84491865cff
denliedesign <customdenlie@gmail.com>
2023-07-21 20:37:41 -0500

    individual class page mobile finished

--- resources/views/age11-12.blade.php
+++ resources/views/age11-12.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="banner-wrap-lg">
+    <div class="banner-wrap-lg d-none d-md-block">
         <div class="banner-lg within-11-12"></div>
     </div>
+
+    <img src="/images/within-11-12.jpg" alt="girl doing headstand" class="img-fluid d-block d-md-none">
 
     <div class="container py-5">
         {{--        <h4 class="text-center">Dance Classes for Preschoolers Ages 2-4</h4>--}}
--- resources/views/age2-4.blade.php
+++ resources/views/age2-4.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="banner-wrap-lg">
+    <div class="banner-wrap-lg d-none d-md-block">
         <div class="banner-lg within-2-4"></div>
     </div>
+    <img src="/images/within-2-4.jpg" alt="girl doing headstand" class="img-fluid d-block d-md-none">
 
     <div class="container py-5">
 {{--        <h4 class="text-center">Dance Classes for Preschoolers Ages 2-4</h4>--}}
