2edafaf44347d682589ead10020cb99a6d0d69e1
denliedesign <customdenlie@gmail.com>
2023-07-31 13:37:48 -0500

    tab title 2nd attempt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'The Pulse | Bedford Hills, NY') }}</title>
+        <title>@yield('title', 'The Pulse | Bedford Hills, NY')</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
