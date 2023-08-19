f60d256f0804164fdf66a75ca0a4df1c0927eeba
denliedesign <customdenlie@gmail.com>
2023-08-18 19:40:47 -0500

    hide asp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('contents', Content::all());
-        View::share('photos', Photo::all());
+//        View::share('photos', Photo::all());
     }
 }
